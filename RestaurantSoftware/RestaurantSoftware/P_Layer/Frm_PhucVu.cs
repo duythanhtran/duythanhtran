eb7b13062033168138690ccc068298ccc1473185
HaHuyDuc <hahuyduc94@gmail.com>
2016-10-05 08:17:18 +0700

    commit len github

---
+++ RestaurantSoftware/RestaurantSoftware/P_Layer/Frm_PhucVu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestaurantSoftware.P_Layer
+{
+    public partial class Frm_PhucVu : Form
+    {
+        public Frm_PhucVu()
+        {
+            InitializeComponent();
+        }
+    }
+}
